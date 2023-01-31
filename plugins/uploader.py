c30119338c3dab68a673dee6f6f0374d24e3bcd9
Abhay3011 <83626456+Abhay3011@users.noreply.github.com>
2023-01-31 13:35:08 +0530

    Update uploader.py

--- plugins/uploader.py
+++ plugins/uploader.py
@@ -105,7 +105,7 @@ async def uploaderHandler(bot:Update, msg:Message):
                             f"During Downloading file from this url: {url}.\n{e}"
                         )
                     else:
-                        if downObj.get_final_filesize() >= 2147483648:
+                        if downObj.get_final_filesize() >= 648:
                             await pmsg.edit_text(
                                 "<b>URL also verifed, Now downloading the file...</b>",
                                 parse_mode = 'html'
